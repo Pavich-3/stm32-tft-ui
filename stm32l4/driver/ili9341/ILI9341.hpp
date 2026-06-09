0c9deec8672bb02d2d37fc13776ceaffbc6b0f47
pavlo.baranchuk <pavlo.baranchuk@goreel.tech>
2026-06-09 22:33:58 +0300

    create basic class ILI9341

--- stm32l4/driver/ili9341/ILI9341.hpp
+++ stm32l4/driver/ili9341/ILI9341.hpp
@@ -1,1 +1,22 @@
 #pragma once
+
+#include "stm32l4xx_hal.h"
+#include <cstdint>
+
+class ILI9341 {
+private:
+	static constexpr uint16_t DC = GPIO_PIN_8;
+	static constexpr uint16_t RESET = GPIO_PIN_9;
+	static constexpr uint16_t CS = GPIO_PIN_10;
+	static constexpr uint16_t LED = GPIO_PIN_11;
+
+	SPI_HandleTypeDef* hspi_;
+
+	void sendCommand(uint8_t cmd);
+	void sendData(uint8_t data);
+
+public:
+	ILI9341(SPI_HandleTypeDef* hspi);
+
+	void init();
+};
